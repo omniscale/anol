35c4d7a61fde0f1d61272c93950f1426693b39b6
Kai Culemann <culemann@omniscale.de>
2014-12-11 14:52:14 +0100

    Removed autoResize functionality from map

--- src/modules/map/map-directive.js
+++ src/modules/map/map-directive.js
@@ -19,18 +19,11 @@ angular.module('anol.map')
  */
 .directive('anolMap', ['DefaultMapName', 'MapService', function(DefaultMapName, MapService) {
     return {
-        scope: {
-            autoResize: '@autoResize'
-        },
+        scope: {},
         link: {
             pre: function(scope, element, attrs) {
                 scope.mapName = DefaultMapName;
                 scope.map = MapService.getMap();
-
-                scope.setContainerHeight = function(height) {
-                    element.css('height', height);
-                    scope.map.updateSize();
-                };
             },
             post: function (scope, element, attrs) {
                 element
@@ -39,22 +32,6 @@ angular.module('anol.map')
 
                 scope.map.setTarget(scope.mapName);
 
-                if(attrs.autoResize==='height' || attrs.autoResize === 'both') {
-                    scope.$watch(function() {
-                        return window.innerHeight;
-                    }, function(newVal, oldVal) {
-                        if(newVal !== undefined) {
-                            // TODO update when support others than px
-                            var marginTop = element.css('margin-top');
-                            marginTop = parseInt(marginTop.slice(0, marginTop.length -2));
-
-                            var marginBottom = element.css('margin-bottom');
-                            marginBottom = parseInt(marginBottom.slice(0, marginBottom.length -2));
-                            // TODO find out why mapHeight needs to be 4 px smaller than it could be
-                            scope.setContainerHeight(newVal - marginTop - marginBottom - 4);
-                        }
-                    });
-                }
             }
         },
         controller: function($scope, $element, $attrs) {
