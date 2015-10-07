a82d0179768c70c28f551070acf813dfccbe9033
Kai Culemann <culemann@omniscale.de>
2015-10-07 11:09:47 +0200

    Implemented styling by feature property 'style' for geojson layer

--- src/anol/layer/staticgeojson.js
+++ src/anol/layer/staticgeojson.js
@@ -11,6 +11,11 @@
  * Inherits from {@link anol.layer.Layer anol.layer.Layer}.
  */
 anol.layer.StaticGeoJSON = function(_options) {
+    if(_options === false) {
+        anol.layer.Layer.call(this);
+        return;
+    }
+    var self = this;
     var defaults = {};
     var options = $.extend({},
         anol.layer.Layer.prototype.DEFAULT_OPTIONS,
@@ -22,9 +27,15 @@ anol.layer.StaticGeoJSON = function(_options) {
         this._createSourceOptions(options.olLayer.source)
     );
 
-    options.olLayer = new ol.layer.Vector(options.olLayer);
+    this.defaultStyle = options.olLayer.style || ol.style.defaultStyleFunction;
 
+    options.olLayer.style = function(feature) {
+        return [self.createStyle(feature)];
+    };
+
+    options.olLayer = new ol.layer.Vector(options.olLayer);
     anol.layer.Layer.call(this, options);
+
 };
 anol.layer.StaticGeoJSON.prototype = new anol.layer.Layer();
 $.extend(anol.layer.StaticGeoJSON.prototype, {
@@ -41,5 +52,155 @@ $.extend(anol.layer.StaticGeoJSON.prototype, {
         srcOptions.format = new ol.format.GeoJSON();
 
         return srcOptions;
+    },
+    createStyle: function(feature, resolution) {
+        if(feature === undefined) {
+            return;
+        }
+        var geometryType = feature.getGeometry().getType();
+        var geojsonStyle = feature.get('style');
+        var defaultStyle = angular.isFunction(this.defaultStyle) ?
+            this.defaultStyle(feature, resolution)[0] : this.defaultStyle;
+
+        if(geojsonStyle === undefined) {
+            return defaultStyle;
+        }
+        if(geometryType === 'Point') {
+            return new ol.style.Style({
+                image: this.createImageStyle(geojsonStyle, defaultStyle.getImage())
+            });
+        } else {
+            // line features ignores fill style
+            return new ol.style.Style({
+                fill: this.createFillStyle(geojsonStyle, defaultStyle.getFill()),
+                stroke: this.createStrokeStyle(geojsonStyle, defaultStyle.getStroke())
+            });
+        }
+    },
+    createImageStyle: function(style, defaultImageStyle) {
+        var isCircle = style.radius !== undefined;
+        var isIcon = style.externalGraphic !== undefined;
+        var isDefaultCircle = defaultImageStyle instanceof ol.style.Circle;
+        var isDefaultIcon = defaultImageStyle instanceof ol.style.Icon;
+
+        if(isCircle || !(isIcon && isDefaultCircle)) {
+            return this.createCircleStyle(style, defaultImageStyle);
+        } else if (isIcon || !(isCircle && isDefaultIcon)) {
+            return this.createIconStyle(style, defaultImageStyle);
+        }
+        return defaultImageStyle;
+    },
+    createCircleStyle: function(style, defaultCircleStyle) {
+        var defaultStrokeStyle = new ol.style.Stroke();
+        var defaultFillStyle = new ol.style.Fill();
+        if(defaultCircleStyle instanceof ol.style.Circle) {
+            defaultStrokeStyle = defaultCircleStyle.getStroke();
+            defaultFillStyle = defaultCircleStyle.getFill();
+        }
+        var radius = defaultCircleStyle.getRadius();
+        if(style.radius !== undefined) {
+            radius = parseFloat(style.radius);
+        }
+        return new ol.style.Circle({
+            radius: radius,
+            stroke: this.createStrokeStyle(style, defaultStrokeStyle),
+            fill: this.createFillStyle(style, defaultFillStyle)
+        });
+    },
+    createIconStyle: function(style, _defaultIconStyle) {
+        var defaultIconStyle = new ol.style.Icon();
+        if(_defaultIconStyle instanceof ol.style.Icon) {
+            defaultIconStyle = _defaultIconStyle;
+        }
+        var src = defaultIconStyle.getSrc();
+        if(style.externalGraphic !== undefined) {
+            src = style.externalGraphic;
+        }
+        var rotation = defaultIconStyle.getRotation();
+        if(style.rotation !== undefined) {
+            rotation = parseFloat(style.rotation);
+        }
+        var scale = defaultIconStyle.getScale();
+        if(style.scale !== undefined) {
+            scale = parseFloat(style.scale);
+        }
+
+        var iconStyle = new ol.style.Icon(({
+            src: src,
+            rotation: rotation
+        }));
+        if(scale === undefined && style.graphicWidth !== undefined) {
+            // get original image size
+            var size = iconStyle.getSize();
+            if (size !== null) {
+                scale = parseInt(style.graphicWidth) / size[0];
+            }
+        }
+        iconStyle.setScale(scale);
+        return iconStyle;
+    },
+    createFillStyle: function(style, defaultFillStyle) {
+        var color = ol.color.asArray(defaultFillStyle.getColor());
+        if (style.fillColor !== undefined) {
+            var fillColor = ol.color.asArray(style.fillColor);
+            color[0] = fillColor[0];
+            color[1] = fillColor[1];
+            color[2] = fillColor[2];
+        }
+        if(style.fillOpacity !== undefined) {
+            color[3] = parseFloat(style.fillOpacity);
+        }
+        return new ol.style.Fill({
+            color: color
+        });
+    },
+    createStrokeStyle: function(style, defaultStrokeStyle) {
+        var color = ol.color.asArray(defaultStrokeStyle.getColor());
+        var strokeWidth = defaultStrokeStyle.getWidth();
+        var strokeDashstyle = defaultStrokeStyle.getLineDash();
+
+        if(style.strokeColor !== undefined) {
+            var strokeColor = ol.color.asArray(style.strokeColor);
+            color[0] = strokeColor[0];
+            color[1] = strokeColor[1];
+            color[2] = strokeColor[2];
+        }
+        if(style.strokeOpacity !== undefined) {
+            color[3] = parseFloat(style.strokeOpacity);
+        }
+        if(style.strokeWidth !== undefined) {
+            strokeWidth = parseFloat(style.strokeWidth);
+        }
+        if(style.strokeDashstyle !== undefined) {
+            strokeDashstyle = this.createDashStyle(strokeWidth, style.strokeDashstyle);
+        }
+
+        return new ol.style.Stroke({
+            color: color,
+            width: strokeWidth,
+            lineDash: strokeDashstyle,
+            lineJoin: 'round'
+        });
+    },
+    // Taken from OpenLayers 2.13.1
+    // see https://github.com/openlayers/openlayers/blob/release-2.13.1/lib/OpenLayers/Renderer/SVG.js#L391
+    createDashStyle: function(strokeWidth, strokeDashstyle) {
+        var w = strokeWidth;
+        var str = strokeDashstyle;
+        switch (str) {
+            case 'dot':
+                return [1, 4 * w];
+            case 'dash':
+                return [4 * w, 4 * w];
+            case 'dashdot':
+                return [4 * w, 4 * w, 1, 4 * w];
+            case 'longdash':
+                return [8 * w, 4 * w];
+            case 'longdashdot':
+                return [8 * w, 4 * w, 1, 4 * w];
+            // also matches 'solid'
+            default:
+                return undefined;
+          }
     }
 });
