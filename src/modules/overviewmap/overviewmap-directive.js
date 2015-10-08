2177635942f484c3c92ae0d5e8fec9ec818cafaf
Kai Culemann <culemann@omniscale.de>
2015-10-08 08:55:41 +0200

    Show only background layers in overview map

--- src/modules/overviewmap/overviewmap-directive.js
+++ src/modules/overviewmap/overviewmap-directive.js
@@ -1,12 +1,18 @@
 angular.module('anol.overviewmap')
 
-.directive('anolOverviewMap', ['ControlsService', function(ControlsService) {
+.directive('anolOverviewMap', ['ControlsService', 'LayersService', function(ControlsService, LayersService) {
     return {
         restrict: 'A',
         scope: {},
         link: function(scope, element, attrs) {
+            var backgroundLayers = [];
+            angular.forEach(LayersService.backgroundLayers, function(layer) {
+                backgroundLayers.push(layer.olLayer);
+            });
             var control = new anol.control.Control({
-                olControl: new ol.control.OverviewMap()
+                olControl: new ol.control.OverviewMap({
+                    layers: backgroundLayers
+                })
             });
             ControlsService.addControl(control);
         }
