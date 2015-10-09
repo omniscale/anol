1cf115964d1f57a5da6824d32de4196eb11aeb48
Kai Culemann <culemann@omniscale.de>
2015-10-09 13:15:24 +0200

    Added placeholder attribute to geocoder searchbox

--- src/modules/geocoder/geocoder-directive.js
+++ src/modules/geocoder/geocoder-directive.js
@@ -17,7 +17,6 @@ angular.module('anol.geocoder')
   function($http, $timeout, MapService, ControlsService) {
     return {
       restrict: 'A',
-      require: '?^anolMap',
       transclude: true,
       templateUrl: function(tElement, tAttrs) {
           var defaultUrl = 'src/modules/geocoder/templates/searchbox.html';
@@ -25,79 +24,86 @@ angular.module('anol.geocoder')
       },
       scope: {
         geocoder: '@anolGeocoderSearchbox',
-        zoomLevel: '@'
+        zoomLevel: '@',
+        placeholder: '@'
       },
-      link: function(scope, element, attrs, AnolMapController) {
-        var geocoder = new anol.geocoder[scope.geocoder]();
+      compile: function(tElement, tAttrs) {
+        var prepareAttr = function(attr, _default) {
+            return attr || _default;
+        };
+        tAttrs.placeholder = prepareAttr(tAttrs.placeholder, 'Search a place');
+        return function(scope, element, attrs) {
+          var geocoder = new anol.geocoder[scope.geocoder]();
 
-        scope.searchResults = [];
+          scope.searchResults = [];
 
-        scope.handleInputKeypress = function(event) {
-          event.stopPropagation();
-          if((event.key === 'ArrowDown' || event.keyCode === 40) && scope.searchResults.length > 0) {
-            event.preventDefault();
+          scope.handleInputKeypress = function(event) {
+            event.stopPropagation();
+            if((event.key === 'ArrowDown' || event.keyCode === 40) && scope.searchResults.length > 0) {
+              event.preventDefault();
+              // use timeout to prevent input element on-blur bug. ($apply already in progress error is raised)
+              $timeout(function() {
+                element.find('.dropdown-menu li a:first').focus();
+              }, 0);
+            }
+            if(event.key === 'Enter' || event.keyCode === 13) {
+              geocoder.request(scope.searchString)
+                .then(function(results) {
+                  scope.$apply(function() {
+                    scope.searchResults = results;
+                    element.find('.anol-searchbox').addClass('open');
+                  });
+                });
+            }
+            return false;
+          };
+
+          scope.handleDropdownKeypress = function(event) {
+            event.stopPropagation();
+            var targetParent = angular.element(event.currentTarget).parent();
+            if(event.key === 'ArrowDown' || event.keyCode === 40) {
+              event.preventDefault();
+              targetParent.next().find('a').focus();
+            }
+            if(event.key === 'ArrowUp' || event.keyCode === 38) {
+              event.preventDefault();
+              var target = targetParent.prev().find('a');
+              if(target.length === 0) {
+                element.find('.form-control').focus();
+              } else {
+                target.focus();
+              }
+            }
+            return false;
+          };
+
+          scope.handleMouseover = function(event) {
             // use timeout to prevent input element on-blur bug. ($apply already in progress error is raised)
             $timeout(function() {
-              element.find('.dropdown-menu li a:first').focus();
+              angular.element(event.currentTarget).focus();
             }, 0);
-          }
-          if(event.key === 'Enter' || event.keyCode === 13) {
-            geocoder.request(scope.searchString)
-              .then(function(results) {
-                scope.$apply(function() {
-                  scope.searchResults = results;
-                  element.find('.anol-searchbox').addClass('open');
-                });
-              });
-          }
-          return false;
-        };
+          };
 
-        scope.handleDropdownKeypress = function(event) {
-          event.stopPropagation();
-          var targetParent = angular.element(event.currentTarget).parent();
-          if(event.key === 'ArrowDown' || event.keyCode === 40) {
-            event.preventDefault();
-            targetParent.next().find('a').focus();
-          }
-          if(event.key === 'ArrowUp' || event.keyCode === 38) {
-            event.preventDefault();
-            var target = targetParent.prev().find('a');
-            if(target.length === 0) {
-              element.find('.form-control').focus();
-            } else {
-              target.focus();
+          scope.showResult = function(result) {
+            var view = MapService.getMap().getView();
+            view.setCenter(
+              ol.proj.transform(
+                result.coordinate,
+                result.projectionCode,
+                view.getProjection()
+              )
+            );
+            if(angular.isDefined(scope.zoomLevel)) {
+              view.setZoom(parseInt(scope.zoomLevel));
             }
-          }
-          return false;
-        };
+            scope.searchResults = [];
+            element.find('.anol-searchbox').removeClass('open');
+          };
 
-        scope.handleMouseover = function(event) {
-          // use timeout to prevent input element on-blur bug. ($apply already in progress error is raised)
-          $timeout(function() {
-            angular.element(event.currentTarget).focus();
-          }, 0);
+          ControlsService.addControl(new anol.control.Control({
+            element: element
+          }));
         };
-
-        scope.showResult = function(result) {
-          var view = MapService.getMap().getView();
-          view.setCenter(
-            ol.proj.transform(
-              result.coordinate,
-              result.projectionCode,
-              view.getProjection()
-            )
-          );
-          if(angular.isDefined(scope.zoomLevel)) {
-            view.setZoom(parseInt(scope.zoomLevel));
-          }
-          scope.searchResults = [];
-          element.find('.anol-searchbox').removeClass('open');
-        };
-
-        ControlsService.addControl(new anol.control.Control({
-          element: element
-        }));
       }
     };
 }]);
