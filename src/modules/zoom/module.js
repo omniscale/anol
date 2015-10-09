060317537e9fc16c641a1fb809933e301bdc97f0
Kai Culemann <culemann@omniscale.de>
2015-10-09 10:37:42 +0200

    Added zoom directive

---
+++ src/modules/zoom/module.js
@@ -0,0 +1,7 @@
+/**
+ * @ngdoc overview
+ * @name anol.zoom
+ * @description
+ * Module providing the zoom directive
+ */
+angular.module('anol.zoom', ['anol.map']);
---
+++ src/modules/zoom/zoom-directive.js
@@ -0,0 +1,35 @@
+angular.module('anol.zoom')
+.directive('anolZoom', ['$compile', 'ControlsService',
+    function($compile, ControlsService) {
+    return {
+        restrict: 'A',
+        scope: {
+            zoomInTooltipText: '@',
+            zoomInTooltipPlacement: '@',
+            zoomOutTooltipText: '@',
+            zoomOutTooltipPlacement: '@'
+        },
+        link: function(scope, element, attrs) {
+            var olControl = new ol.control.Zoom({
+                zoomInLabel: document.createTextNode(''),
+                zoomOutLabel: document.createTextNode('')
+            });
+            var control = new anol.control.Control({
+                olControl: olControl
+            });
+            var zoomInButton = angular.element(olControl.element).find('.ol-zoom-in');
+            zoomInButton.removeAttr('title');
+            zoomInButton.attr('tooltip', scope.zoomInTooltipText || 'Zoom in');
+            zoomInButton.attr('tooltip-placement', scope.zoomInTooltipPlacement || 'right');
+            $compile(zoomInButton)(scope);
+
+            var zoomOutButton = angular.element(olControl.element).find('.ol-zoom-out');
+            zoomOutButton.removeAttr('title');
+            zoomOutButton.attr('tooltip', scope.zoomOutTooltipText || 'Zoom out');
+            zoomOutButton.attr('tooltip-placement', scope.zoomOutTooltipPlacement || 'right');
+            $compile(zoomOutButton)(scope);
+
+            ControlsService.addControl(control);
+        }
+    };
+}]);
