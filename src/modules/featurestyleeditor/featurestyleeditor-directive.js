a5207cce4d1e1ec8ecf81f5d7bc2e66c915ceecb
Kai Culemann <culemann@omniscale.de>
2015-12-22 11:52:56 +0100

    Refactored feature style editor directive

--- src/modules/featurestyleeditor/featurestyleeditor-directive.js
+++ src/modules/featurestyleeditor/featurestyleeditor-directive.js
@@ -4,104 +4,81 @@ angular.module('anol.featurestyleeditor')
  * @name anol.featurestyleeditor.directive:anolFeatureStyleEditor
  *
  * @restrict A
- * @requires $modal
+ * @requires $rootScope
+ * @requires $translate
  *
  * @param {string} templateUrl Url to template to use instead of default one
- * @param {string} modalTemplateUrl Url to template to use for modal instead of default one
+ * @param {ol.Feature} anolFeatureStyleEditor Feature to edit
  *
  * @description
- * Shows a modal for editing feature style
+ * Shows a form for editing feature style depending on its geometry type
  */
-.directive('anolFeatureStyleEditor', ['$modal', function($modal) {
+.directive('anolFeatureStyleEditor', ['$rootScope', '$translate', function($rootScope, $translate) {
+    var prepareStyleProperties = function(_style) {
+        var style = angular.copy(_style);
+        if(style.radius !== undefined) {
+            style.radius = parseInt(style.radius);
+        }
+        if(style.strokeWidth !== undefined) {
+            style.strokeWidth = parseInt(style.strokeWidth);
+        }
+        if(style.strokeOpacity !== undefined) {
+            style.strokeOpacity = parseFloat(style.strokeOpacity);
+        }
+        if(style.fillOpacity !== undefined) {
+            style.fillOpacity = parseFloat(style.fillOpacity);
+        }
+        return style;
+    };
+
     return {
         restrict: 'A',
         scope: {
-            modalTemplateUrl: '@'
+            feature: '=anolFeatureStyleEditor'
         },
         templateUrl: function(tElement, tAttrs) {
             var defaultUrl = 'src/modules/featurestyleeditor/templates/featurestyleeditor.html';
             return tAttrs.templateUrl || defaultUrl;
         },
-        transclude: true,
         link: function(scope, element, attrs) {
-            scope.openEditor = function() {
-                var defaultModalUrl = 'src/modules/featurestyleeditor/templates/featurestyleeditor-modal.html';
-                var modalInstance = $modal.open({
-                    templateUrl: scope.modalTemplateUrl || defaultModalUrl,
-                    controller: 'FeatureStyleEditorModalController',
-                    resolve: {
-                        style: function () {
-                            return scope.feature.getProperties().style || {};
-                        },
-                        geometryType: function() {
-                            return scope.feature.getGeometry().getType();
-                        }
-                    }
-                });
-                modalInstance.result.then(function(style) {
-                    if(!angular.equals(style, {})) {
-                        scope.feature.set('style', style);
-                    }
-                    scope.feature = undefined;
+            var styleWatcher;
+            scope.$watch('feature', function(feature) {
+                if(styleWatcher !== undefined) {
+                    styleWatcher();
+                    styleWatcher = undefined;
+                }
+                if(feature !== undefined) {
+                    scope.style = prepareStyleProperties(
+                        feature.get('style') || {}
+                    );
+                    scope.geometryType = feature.getGeometry().getType();
+                    styleWatcher = scope.$watchCollection('style', function(style) {
+                        feature.set('style', style);
+                    });
+                }
+            });
+
+            var translate = function() {
+                $translate([
+                    'anol.featurestyleeditor.SOLID',
+                    'anol.featurestyleeditor.DOT',
+                    'anol.featurestyleeditor.DASH',
+                    'anol.featurestyleeditor.DASHDOT',
+                    'anol.featurestyleeditor.LONGDASH',
+                    'anol.featurestyleeditor.LONGDASHDOT'
+                ]).then(function(translations) {
+                    scope.strokeDashStyles = [
+                        {value: 'solid', label: translations['anol.featurestyleeditor.SOLID']},
+                        {value: 'dot', label: translations['anol.featurestyleeditor.DOT']},
+                        {value: 'dash', label: translations['anol.featurestyleeditor.DASH']},
+                        {value: 'dashdot', label: translations['anol.featurestyleeditor.DASHDOT']},
+                        {value: 'longdash', label: translations['anol.featurestyleeditor.LONGDASH']},
+                        {value: 'longdashdot', label: translations['anol.featurestyleeditor.LONGDASHDOT']}
+                    ];
                 });
             };
-        },
-        controller: function($scope, $element, $attrs) {
-            this.editFeature = function(feature) {
-                $scope.feature = feature;
-                $scope.openEditor();
-            };
+            $rootScope.$on('$translateChangeSuccess', translate);
+            translate();
         }
     };
-}])
-/**
- * @ngdoc controller
- * @name anol.featurestyleeditor.controller:anolFeatureStyleEditorModalController
- *
- * @restrict A
- * @requires $rootScope
- * @requires $scope
- * @requires $modalInstance
- * @requires $translate
- * @requires style
- * @requires geometryType
- *
- * @description
- * Controller for style editor modal
- */
-.controller('FeatureStyleEditorModalController', ['$rootScope', '$scope', '$modalInstance', '$translate', 'style', 'geometryType',
-    function($rootScope, $scope, $modalInstance, $translate, style, geometryType) {
-        $scope.style = style;
-        $scope.geometryType = geometryType;
-
-        var translate = function() {
-            $translate([
-                'anol.featurestyleeditor.SOLID',
-                'anol.featurestyleeditor.DOT',
-                'anol.featurestyleeditor.DASH',
-                'anol.featurestyleeditor.DASHDOT',
-                'anol.featurestyleeditor.LONGDASH',
-                'anol.featurestyleeditor.LONGDASHDOT'
-            ]).then(function(translations) {
-                $scope.strokeDashStyles = [
-                    {value: 'solid', label: translations['anol.featurestyleeditor.SOLID']},
-                    {value: 'dot', label: translations['anol.featurestyleeditor.DOT']},
-                    {value: 'dash', label: translations['anol.featurestyleeditor.DASH']},
-                    {value: 'dashdot', label: translations['anol.featurestyleeditor.DASHDOT']},
-                    {value: 'longdash', label: translations['anol.featurestyleeditor.LONGDASH']},
-                    {value: 'longdashdot', label: translations['anol.featurestyleeditor.LONGDASHDOT']}
-                ];
-            });
-        };
-
-        $rootScope.$on('$translateChangeSuccess', translate);
-        translate();
-
-
-        $scope.ok = function () {
-            $modalInstance.close($scope.style);
-        };
-        $scope.cancel = function () {
-            $modalInstance.dismiss('cancel');
-        };
 }]);
