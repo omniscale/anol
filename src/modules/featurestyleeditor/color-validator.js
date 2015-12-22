e32a25c674c535e57f59a2a8676ce6f237307c52
Kai Culemann <culemann@omniscale.de>
2015-12-22 14:08:02 +0100

    Added color validator and used it in feature style directive template

---
+++ src/modules/featurestyleeditor/color-validator.js
@@ -0,0 +1,20 @@
+angular.module('anol.featurestyleeditor')
+
+.directive('color', function() {
+    var COLOR_REGEX = /^#[0-9a-fA-F]{3}([0-9a-fA-F]{3})?$/;
+    return {
+        require: 'ngModel',
+        link: function(scope, elem, attrs, ctrl) {
+            ctrl.$validators.color = function(modelValue, viewValue) {
+                if(ctrl.$isEmpty(modelValue)) {
+                    return false;
+                }
+                if(ctrl.$isEmpty(viewValue)) {
+                    return false;
+                }
+                var result =  COLOR_REGEX.test(viewValue);
+                return result;
+            };
+        }
+    };
+});
