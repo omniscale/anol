94883b8eff3c3128ad529470451f6ddcd9043629
Kai Culemann <culemann@omniscale.de>
2016-01-05 14:12:53 +0100

    Changed dpi for calculating scale

--- src/modules/scale/scaletext-directive.js
+++ src/modules/scale/scaletext-directive.js
@@ -10,7 +10,7 @@ angular.module('anol.scale')
  */
 .constant('calculateScale', function(view) {
     var INCHES_PER_METER = 1000 / 25.4;
-    var DPI = 72;
+    var DPI = 91;
     // found at https://groups.google.com/d/msg/ol3-dev/RAJa4locqaM/4AzBrkndL9AJ
     var resolution = view.getResolution();
     var mpu = view.getProjection().getMetersPerUnit();
