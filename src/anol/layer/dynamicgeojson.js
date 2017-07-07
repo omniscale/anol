fe84dff66ab89dbb80f24dfd78988cbc4fea1b87
Kai Culemann <culemann@omniscale.de>
2017-07-07 17:40:32 +0200

    Added refresh method to DynamicGeoJson layer

--- src/anol/layer/dynamicgeojson.js
+++ src/anol/layer/dynamicgeojson.js
@@ -135,5 +135,9 @@ $.extend(anol.layer.DynamicGeoJSON.prototype, {
             return new ol.style.Style();
         }
         return anol.layer.StaticGeoJSON.prototype.createStyle.call(this, feature, resolution);
+    },
+    refresh: function() {
+        this.olSource.clear();
+        this.olSource.refresh();
     }
 });
