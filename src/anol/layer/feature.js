b21e94fe04ef70f48b00b5015b77832e5e59ac50
Kai Culemann <culemann@omniscale.de>
2017-08-23 13:28:21 +0200

    Clear source instead of refresh when clustered layer removed

--- src/anol/layer/feature.js
+++ src/anol/layer/feature.js
@@ -156,15 +156,14 @@ $.extend(anol.layer.Feature.prototype, {
             if(unclusteredIdx > -1) {
                 unclusteredAnolLayers.splice(unclusteredIdx, 1);
                 this.unclusteredSource.set('anolLayers', unclusteredAnolLayers);
-                this.unclusteredSource.refresh();
             }
             var anolLayers = this.olLayer.getSource().get('anolLayers');
             var idx = anolLayers.indexOf(this);
             if(idx > -1) {
                 anolLayers.splice(idx, 1);
                 this.olLayer.getSource().set('anolLayers', anolLayers);
-                this.olLayer.getSource().refresh();
             }
+            this.olSource.clear(true);
         }
         anol.layer.Layer.prototype.removeOlLayer.call(this);
     },
