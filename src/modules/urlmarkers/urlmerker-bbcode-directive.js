c446295ee068b4d69f4b2bcff89e17ce72275242
Kai Culemann <culemann@omniscale.de>
2017-09-14 15:25:59 +0200

    Added simple bbcode to url marker label

---
+++ src/modules/urlmarkers/urlmerker-bbcode-directive.js
@@ -0,0 +1,26 @@
+angular.module('anol.urlmarkers')
+.directive('bbcode', [function() {
+    var snippets = {
+        'b': '<b>$1</b>',
+        'u': '<u>$1</u>',
+        'i': '<i>$1</i>'
+    };
+
+    return {
+        link: function(scope, element, attrs) {
+            scope.$watch(function() {
+                var contents = element.html().replace(/^\s+|\s+$/i, '');
+
+                for(var i in snippets) {
+                    var regexp = new RegExp('\\[' + i + '\\](.+?)\\[\/' + i.replace(/[^a-z]/g, '') + '\\]', 'gi');
+
+                    contents = contents.replace(regexp, snippets[i]);
+                }
+
+                contents = contents.replace(new RegExp('\\[br\\]'), '<br>');
+
+                element.html(contents);
+            });
+        }
+    };
+}]);
