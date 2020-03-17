413ad28d3593143b2c4df0cc8996808dbea4bc9d
Dominik Helle <helle@omniscale.de>
2020-03-17 16:43:14 +0100

    projects: add waiting class on save and load settings

--- src/modules/savesettings/savesettings-directive.js
+++ src/modules/savesettings/savesettings-directive.js
@@ -25,15 +25,26 @@ angular.module('anol.savesettings')
                         });
                     }    
 
+                    var pageBody = angular.element(document).find('body');
+                    scope.addWaiting = function() {
+                        pageBody.addClass('waiting');
+                    };
+                    scope.removeWaiting = function() {
+                        pageBody.removeClass('waiting');
+                    };
+
                     scope.projectSettings = ProjectSettings;
                     scope.close = function() {
                         scope.modalCallBack();
                     };
                     scope.delete = function(id) {
+                        scope.addWaiting();
                         SaveSettingsService.delete(id).then(function(data) {
                             NotificationService.addInfo(data.message);
+                            scope.removeWaiting();
                         }, function(data) {
                             NotificationService.addError(data.message);
+                            scope.removeWaiting();
                         });
                     };
                     scope.save = function(name) {
@@ -48,22 +59,27 @@ angular.module('anol.savesettings')
                         if (angular.isUndefined(name) || name == '') {
                             return;
                         }
+                        scope.addWaiting();
                         SaveSettingsService.save(name).then(function(data) {
                             scope.modalCallBack();
                             NotificationService.addInfo(data.message);
+                            scope.removeWaiting();
                         }, function(data) {
                             NotificationService.addError(data.message);
+                            scope.removeWaiting();
                         });
                     };
                     scope.load = function(id) {
                         if (angular.isUndefined(id)) {
                             return;
                         }
+                        scope.addWaiting();
                         SaveSettingsService.load(id).then(function(data) {
                             scope.modalCallBack();
                             NotificationService.addInfo(data.message);
                         }, function(data) {
                             NotificationService.addError(data.message);
+                            scope.removeWaiting();
                         });
                     };                 
                 }
